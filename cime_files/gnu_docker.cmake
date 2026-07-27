16037c24a2f7c846510f615e99ebe18665f6c64b
Rich Fiorella <rfiorella@lanl.gov>
2026-07-27 10:07:44 -0600

    Add MOAB_DIR configuration to CIME files
    Set MOAB_DIR=/usr in config_machines.xml and gnu_docker.cmake to enable
    CMake to find MOAB's package configuration files (MOABConfig.cmake).
    This resolves CMake find_package(MOAB) errors when building E3SM with
    the MOAB coupling infrastructure installed in the container.

--- cime_files/gnu_docker.cmake
+++ cime_files/gnu_docker.cmake
@@ -13,3 +13,4 @@ string(APPEND SLIBS " -L$ENV{HDF5_HOME}/lib -lhdf5_fortran -lhdf5 -lhdf5_hl -lhd
 string(APPEND SLIBS " -L$ENV{NETCDF_PATH}/lib/ -lnetcdff -lnetcdf -lcurl -lblas -llapack")
 set(HDF5_PATH "$ENV{HDF5_HOME}")
 set(NETCDF_PATH "$ENV{NETCDF_PATH}")
+set(MOAB_DIR "$ENV{MOAB_DIR}")
